84efa3526098798779a16a2a12b329d42e6baeb3
tacusin <anyoneshero@Hotmail.com>
2024-08-18 07:10:07 -0500

    Update sw.js

--- Resonite/ResoBrowser/sw.js
+++ Resonite/ResoBrowser/sw.js
@@ -1,52 +1,53 @@
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
-const PRECACHE_URLS = [
-
-];
+const CACHE_NAME = 'network-first-cache-v1';
 
 self.addEventListener('install', event => {
+  // Perform install steps
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        // You can add URLs to cache here if needed
+        // return cache.addAll([...]);
+      })
   );
 });
 
 self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
-  // The "only-if-cached" mode can only be used if the request's mode is "same-origin"
-  // There is an issue with chrome where if the devtools are opened it will throw errors 
-  // because the devtools are using an 'only-if-cached' event while not using same origin
-  // https://github.com/paulirish/caltrainschedule.io/issues/49
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=1098389
-  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
-  
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
         }
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
+
+        // Clone the response
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
           });
-        });
+
+        return response;
       })
-    );
-  }
+      .catch(() => {
+        // If the network request fails, try to get it from the cache
+        return caches.match(event.request);
+      })
+  );
 });
